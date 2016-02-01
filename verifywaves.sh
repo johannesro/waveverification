b9975257f9f69a492c89e56dccebf4b4e049f02a
Johannes Rohrs <johannesro@met.no>
2016-02-01 11:36:27 +0100

    remove R interface

--- verifywaves.sh
+++ verifywaves.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 
-export PYTHONPATH="$PYTHONPATH:/home/johannesro/.fou_python/lib/python2.7/site-packages"
-export PYTHONPATH="$PYTHONPATH:/home/johannesro/python"
-export R_LIBS_SITE="/metno/vvfelles/pakker/R-Pakker"
-export R_LIBS="/disk1/local/R-packages"
+#export PYTHONPATH="$PYTHONPATH:/home/johannesro/.fou_python/lib/python2.7/site-packages"
+#export PYTHONPATH="$PYTHONPATH:/home/johannesro/python"
+#export R_LIBS_SITE="/metno/vvfelles/pakker/R-Pakker"
+#export R_LIBS="/disk1/local/R-packages"
 
 printf -v year %2.2i `date +%Y`
 printf -v month %2.2i `date +%m`
 
-cd /home/johannesro/cronjobs/waveverification
+cd /vol/hindcast3/waveverification/waveverification
 
 echo 'start wave verification script' >> wv.log
 
@@ -17,28 +17,28 @@ echo 'start wave verification script' >> wv.log
 ./collectdata.py 
 
 # make plots
-./validate.py
+#./validate.py
 
 # update webpage
-cd /vol/hindcast3/waveverification/website 
+#cd /vol/hindcast3/waveverification/website 
 
 # september is not working with the above commands because the shell misinterprets the lines !?
 #./makepage.sh $year 09
 #./makepage_DD.sh $year 09
 #./makeindex.sh $year 09
 
-./makepage.sh $year $month
-./makepage_DD.sh $year $month
-./makeindex.sh $year $month
+#./makepage.sh $year $month
+#./makepage_DD.sh $year $month
+#./makeindex.sh $year $month
 
 
 # upload webpage
-cd /vol/hindcast3/waveverification
-./upload.sh
+#cd /vol/hindcast3/waveverification
+#./upload.sh
 
 # secure data on local pc
-cd /disk4/waveverification/
-rsync -ua /vol/hindcast3/waveverification/data .
+#cd /disk4/waveverification/
+#rsync -ua /vol/hindcast3/waveverification/data .
 
 
 
