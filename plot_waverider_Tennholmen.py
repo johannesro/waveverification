be54694b657a3d8e4464f00b54febc17064006d1
johannesro <johannesro@met.no>
2018-02-21 11:50:54 +0000

    increase memory for cronjobs

--- plot_waverider_Tennholmen.py
+++ plot_waverider_Tennholmen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2.7
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,13 +9,15 @@ from matplotlib.dates import DayLocator, HourLocator, DateFormatter
 import os
 import METread
 
+print 'hallo'
+
 #### Read Hs and Tz 
 #y, m, d, H, M, Hs, Tz = np.loadtxt('/home/kaihc/DATAWELL/ASCII/waveriderdata.txt', skiprows=1, unpack=True)
 today = datetime.today()
 url = '157.249.178.22/datawell/waved/Tennholmen/%i/%2.2i/' % (today.year, today.month)
 filename = 'Tennholmen{0x324}%i-%2.2i.csv' % (today.year, today.month)
-location = (68.21 , 14.48 )
-
+#location = (68.21 , 14.48 )
+location = (67.3406, 13.5618)
 os.system('rm ' + filename)
 os.system('wget ' + url + filename)
 timeseconds, Hs, Tz = np.loadtxt(filename, skiprows=1, usecols=(2,3,4), unpack=True)
@@ -50,11 +52,11 @@ dteLOCALplot = dteLOCAL[-N:]
 
 ### fetch model data
 modelinterval = 6
-modelrun = dteUTC[-1] - timedelta(hours=dteUTC[-1].hour % modelinterval)
+modelrun = today - timedelta(hours=today.hour % modelinterval) 
 try:
     wam = METread.MWAM4_modrun(location, modelrun, ['Hs', 'Tp'])
-except IOError:
-    modelrun = dteUTC[-1] - timedelta(hours=dteUTC[-1].hour % modelinterval - timedelta(hours=modelinterval))
+except np.isnan(wam['Hs']).all():
+    modelrun = today - (timedelta(hours=today.hour % modelinterval)) - timedelta(hours=modelinterval) 
     wam = METread.MWAM4_modrun(location, modelrun, ['Hs', 'Tp'])
 
 wamtimeUTC  = [wam['time'][i].replace(tzinfo=UTC) for i in range(len(wam['time']))]
@@ -64,14 +66,15 @@ wamtimeLOCAL = [wamtimeUTC[i].astimezone(HERE) for i in range(len(wam['time']))]
 fig = plt.figure()
 
 ax1 = fig.add_subplot(111)
-ax1.plot(dteLOCALplot, HsPlot, 'bd-', linewidth = 3)
-ax1.plot(wamtimeLOCAL, wam['Hs'], 'b--', linewidth = 3)
+ax1.plot(dteLOCALplot, HsPlot, 'bd-', linewidth = 3, label='waverider')
+ax1.plot(wamtimeLOCAL, wam['Hs'], 'b--', linewidth = 3, label='forecast')
+plt.legend(loc='lower right')
 
 xlabelStr = "Local time (" + zoneName + ")"
 xlab = ax1.set_xlabel(xlabelStr, fontsize = 14)
 
 # Make the y-axis label and tick labels match the line color.
-ax1.set_ylabel('Significant wave height [m]', fontsize = 14, color='b')
+ax1.set_ylabel(r'Significant wave height $H_s$ [m]', fontsize = 14, color='b')
 for tl in ax1.get_yticklabels():
     tl.set_color('b')
 
@@ -79,19 +82,19 @@ ax2 = ax1.twinx()
 ax2.plot(dteLOCALplot, TzPlot, 'gd-', linewidth = 3)
 ax2.plot(wamtimeLOCAL, wam['Tm02'], 'g--', linewidth = 3)
 
-ax2.set_ylabel('Mean wave period [s]', fontsize = 14, color='g')
+ax2.set_ylabel(r'Mean wave period $T_{m02}$ [s]', fontsize = 14, color='g')
 
 # set axes limits
-ax1.set_ylim([0, 1.3*np.max((3 ,np.max(HsPlot)))])
-ax2.set_ylim([0, 1.1*np.max((10,np.max(TzPlot)))])
-ax1.set_xlim([dteLOCALplot[0],dteLOCALplot[-1]+timedelta(hours=12)])
+ax1.set_ylim([0, 1.3*np.max([np.max(wam['Hs'])  ,np.max(HsPlot)]) ])
+ax2.set_ylim([0, 1.1*np.max([np.max(wam['Tm02']),np.max(TzPlot)]) ])
+ax1.set_xlim([today-timedelta(hours=12),today+timedelta(hours=12)])
 
 for tl in ax2.get_yticklabels():
     tl.set_color('g')
 
 
 
-titlestr = "Tennholmen waverider"
+titlestr = "Vestfjorden waverider"
 
 # Plot every third hour with minor ticks, but remove at midnight
 
@@ -103,12 +106,12 @@ ax1.xaxis.set_major_formatter(dayfmt)
 
 plt.title(titlestr, fontsize = 20)
 
-fig.autofmt_xdate()
+#fig.autofmt_xdate()
 ax1.grid('on', axis='x', which='both')
 
 plt.savefig('Tennholmen_ts.png', bbox_extra_artists=(xlab,), bbox_inches='tight')
 
-os.system('rsync Tennholmen_ts.png projects.met.no:/var/www/waverider/.')
+os.system('rsync -u Tennholmen_ts.png projects.met.no:/var/www/waverider/.')
 
 #plt.show()
 
